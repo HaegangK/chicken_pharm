7639eb3831c33d21f336187a635e80e11cfda7ff
[AI10] 임지영 <dlawldud021@naver.com>
2024-07-16 17:28:23 +0900

    ✨ Feat: 메인페이지 Home -> Main

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,18 @@ Author : 민선옥
 History
 Date        Author   Status    Description
 2024.07.16  민선옥   Created
-*/ 
+2024.07.16  임지영   Modified    Home -> Main(메인페이지)   
+*/
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/home/Home";
-import Search from "./components/search/Search";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './components/main/Main';
+import Search from './components/search/Search';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Main />} />
         <Route path="/search" element={<Search />} />
       </Routes>
     </Router>
