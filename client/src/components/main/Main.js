bb78f3d23328f642103f3479aa70fca1fcfe0b3a
[AI10] 임지영 <dlawldud021@naver.com>
2024-07-17 12:56:30 +0900

    💄 Design: Nav 추가

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -6,11 +6,13 @@ Author : 임지영
 History
 Date        Author   Status    Description
 2024.07.16  임지영   Created
+2024.07.17  임지영   Modified    Nav 추가
 */
 
 import styled from 'styled-components';
 import MainHeader from './MainHeader';
 import CardNews from './CardNews';
+import Nav from '../Nav';
 
 const MainContainer = styled.div`
   width: 100vw;
@@ -21,6 +23,7 @@ const Main = () => {
     <MainContainer>
       <MainHeader />
       <CardNews />
+      <Nav />
     </MainContainer>
   );
 };
--- client/src/components/mobileCardNews/News.js
+++ client/src/components/mobileCardNews/News.js
@@ -6,6 +6,7 @@ Author : 임지영
 History
 Date        Author   Status    Description
 2024.07.16  임지영   Created
+2024.07.17  임지영   Modified    Nav 추가
 */
 
 import styled from 'styled-components';
@@ -13,6 +14,7 @@ import Header from '../Header';
 import NewsTitle from './NewsTitle';
 import NewsSlide from './NewsSlide';
 import NewsDescription from './NewsDescription';
+import Nav from '../Nav';
 
 const NewsContainer = styled.div`
   width: 100%;
@@ -26,6 +28,7 @@ const News = ({ num }) => {
       <NewsTitle num={num} />
       <NewsSlide num={num} />
       <NewsDescription num={num} />
+      <Nav />
     </NewsContainer>
   );
 };
